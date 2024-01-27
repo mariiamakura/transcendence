f6fd256a53f172f919e9e6153ab2a88b4431a35b
mars12161 <108153152+mars12161@users.noreply.github.com>
2024-01-27 15:47:13 +0100

    separated files for front and backend, created basic structure for microservices game logic and user management

--- transcendence_backend/urls.py
+++ transcendence_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pong_project project.
+URL configuration for transcendence_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,15 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-# pong_project/urls.py
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pong/', include('pong_app.urls')),
 ]
-
-
--- user_management/apps.py
+++ user_management/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PongAppConfig(AppConfig):
+class UserManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pong_app'
+    name = 'user_management'
